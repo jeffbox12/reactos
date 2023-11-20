4ae4516e2fbe8b09b4e219b086a58566c92e2dc5
Splitwirez <Splitwirez@gmail.com>
2023-11-20 15:42:35 -0500

    [NTUSER] Display DLL export version in desktop watermark

--- win32ss/user/ntuser/desktop.c
+++ win32ss/user/ntuser/desktop.c
@@ -1847,6 +1847,7 @@ IntFreeDesktopHeap(IN OUT PDESKTOP Desktop)
 #endif
 }
 
+static BOOL needsDllExportVersionAppended = TRUE;
 BOOL FASTCALL
 IntPaintDesktop(HDC hDC)
 {
@@ -2179,6 +2180,28 @@ IntPaintDesktop(HDC hDC)
         {
             Status = STATUS_SUCCESS;
         }
+
+        if (needsDllExportVersionAppended)
+        {
+            static WCHAR wszExportVer[1024];
+            int index = 2;
+            int major = DLL_EXPORT_VERSION >> 8;
+            int minor = DLL_EXPORT_VERSION & 0x00ff;
+            
+            WCHAR wszDllExportFormat[] = L"Exporting NT %u.%u, %ls";
+            LPCWSTR pszPrevText = VerStrs[index].lpstr;
+            if (!pszPrevText)
+                pszPrevText = L"";
+            
+            if (swprintf(wszExportVer, wszDllExportFormat, major, minor, pszPrevText) > 0)
+            {
+                VerStrs[index].n = lstrlenW(wszExportVer);
+                VerStrs[index].lpstr = wszExportVer;
+            }
+            
+            needsDllExportVersionAppended = FALSE;
+        }
+
         if (NT_SUCCESS(Status) && *wszzVersion)
         {
             if (!InSafeMode)
